dd3749131e74083dd6aed730ae6a255421bc66fc
TocxiSeifo <seifogamer391@gmail.com>
2025-12-28 18:40:08 +0200

    #

--- src/components/FirstSection.jsx
+++ src/components/FirstSection.jsx
@@ -21,32 +21,32 @@ import { useState } from 'react'
 
 const image = [
     {
-        img: img1,
-        id:1
+      img: img1,
+      id:1
     },
     {
-        img: img2,
-        id:2
+      img: img2,
+      id:2
     },
     {
-        img: img3,
-        id:3
+      img: img3,
+      id:3
     },
     {
-        img: img4,
-        id:4
+      img: img4,
+      id:4
     },
     {
-        img: img5,
-        id:5
+      img: img5,
+      id:5
     },
     {
-        img: img6,
-        id:6
+      img: img6,
+      id:6
     },
     {
-        img: img7, 
-        id:7
+      img: img7, 
+      id:7
     },
 ]
 
@@ -55,35 +55,35 @@ export default function SectionOne() {
     const [activeBtn, setActiveBtn] = useState(1);
     return(
         <Reveal>
-            <section className='w-full md:w-full h-314.5 flex flex-col  items-center'>
-                <motion.div  className="bg-bg-color h-149.75 w-full flex flex-col justify-center items-center gap-32 md:flex-col  md:justify-center pt-24">
-                    <div className='flex flex-row font-custom-font h-80 w-full justify-center align-middle  font-semibold gap-12'>
+            <section className='w-full h-auto md:h-314.5 flex flex-col  items-center'>
+                <motion.div  className="bg-bg-color  h-auto md:h-149.75 w-full flex flex-col justify-center items-center lg:gap-32 md:gap-24  md:flex-col  md:justify-center pt-24">
+                    <div className='flex flex-col-reverse h-auto md:flex-col-reverse lg:flex-row  font-custom-font lg:w-[95%] lg:h-80 w-full justify-center items-center font-semibold lg:gap-38 gap-24 md:h-162.5'>
                         <motion.div initial={{opacity:0 , y:100}} whileInView={{ opacity: 1, y: 0 }}
-                            viewport={{ once: true, amount: 0.3 }} transition={{duration:1 , ease:'easeOut' ,delay:0.2}} className='h-57.5 w-164.25 flex flex-col gap-2.5'>
-                            <motion.h1 initial={{opacity:0 , x:-100}} whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true, amount: 0.3 }} transition={{duration:1 , ease:'easeOut' ,delay:0.3}} className='text-text-color leading-19 md:text-[64px] font-semibold'>Lessons and insights</motion.h1>
-                            <motion.span initial={{opacity:0 , x:100}} whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true, amount: 0.3 }} transition={{duration:1 , ease:'easeOut' ,delay:0.5}} className='text-main-green text-4x1 leading-19 md:text-[64px]  font-semibold relative bottom-5'>from 8 years</motion.span>
-                            <motion.p initial={{opacity:0 , y:-100}} whileInView={{ opacity: 1, y: 0 }}
-                            viewport={{ once: true, amount: 0.3 }} transition={{duration:1 , ease:'easeOut' ,delay:0.8}} className='text-p-color'>Where to grow your business as a photographer: site or social media?</motion.p>
-                            <button className='bg-main-green md:h-13 md:w-34 rounded-sm text-gray-100 hover:bg-green-800 hover:duration-500 duration-500 transition-all cursor-pointer active:bg-green-800 p-3.5 mt-7 '>Register</button>
+                          viewport={{ once: true, amount: 0.3 }} transition={{duration:1 , ease:'easeOut' ,delay:0.2}} className=' items-center text-center lg:text-start lg:items-start  lg:gap-0 justify-center mt-15 lg:h-57.5 lg:w-164.25 flex flex-col gap-4'>
+                          <motion.h1 initial={{opacity:0 , x:-100}} whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true, amount: 0.3 }} transition={{duration:1 , ease:'easeOut' ,delay:0.3}} className='text-text-color md:leading-19 text-3xl md:text-5xl lg:text-[64px] font-semibold'>Lessons and insights</motion.h1>
+                          <motion.span initial={{opacity:0 , x:100}} whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true, amount: 0.3 }} transition={{duration:1 , ease:'easeOut' ,delay:0.5}} className='text-main-green text-4x1 md:leading-19 text-3xl md:text-5xl lg:text-[64px]  font-semibold relative lg:bottom-3  md:bottom-12 '>from 8 years</motion.span>
+                          <motion.p initial={{opacity:0 , y:-100}} whileInView={{ opacity: 1, y: 0 }}
+                          viewport={{ once: true, amount: 0.3 }} transition={{duration:1 , ease:'easeOut' ,delay:0.8}} className='text-p-color md:absolute -bottom-12 lg:bottom-0 lg:relative   md:text-[14px]'>Where to grow your business as a photographer: site or social media?</motion.p>
+                          <button className='bg-main-green w-36.25 md:relative lg:relative lg:bottom-0 md:bottom-14   md:h-13 md:w-34 rounded-sm text-gray-100 hover:bg-green-800 hover:duration-500 duration-500 transition-all cursor-pointer active:bg-green-800 p-3.5 mt-7 mb-4 '>Register</button>
                         </motion.div>
-                        <div className='w-97.75 h-101.75'>
-                            <motion.img initial={{opacity:0 , x:100}} whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true, amount: 0.3 }} transition={{duration:1 , ease:'easeOut' ,delay:0.8}} src={img} alt="" className='w-97.75 h-101.75 relative bottom-10 '/>
+                        <div className='h-68 w-[85%]  md:mt-2 md:h-62.5   lg:w-97.75 md:text-center  lg:h-101.75'>
+                            <motion.img initial={{opacity:0 , x:100}} whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true, amount: 0.3 }} transition={{duration:1 , ease:'easeOut' ,delay:0.8}} src={img} alt="" className='w-97.75 h-101.75 relative lg:bottom-10 xl:bottom-3 sm:left-22 md:left-28  lg:left-0 object-contain'/>
                         </div>
                     </div>
-                    <div className='flex flex-row w-11.5 h-2.5 gap-2'>
+                    <div className='flex flex-row w-11.5 h-2.5 gap-2 mb-2.5 md:relative md:bottom-32 lg:bottom-0'>
                         <button className={activeBtn === 1 ? 'h-2.5 w-2.5 bg-main-green rounded-[50%]': 'h-2.5 w-2.5 bg-green-200 rounded-[50%]'} onClick={() => setActiveBtn(1)}></button>
                         <button className={activeBtn === 2 ? 'h-2.5 w-2.5 bg-main-green rounded-[50%]': 'h-2.5 w-2.5 bg-green-200 rounded-[50%]'}  onClick={() => setActiveBtn(2)}></button>
                         <button className={activeBtn === 3 ? 'h-2.5 w-2.5 bg-main-green rounded-[50%]': 'h-2.5 w-2.5 bg-green-200 rounded-[50%]'}   onClick={() => setActiveBtn(3)}></button>
                     </div>
                 </motion.div>
-                <div className='flex flex-col justify-center items-center w-full h-47.5 mt-64 gap-4'>
+                <div className='flex flex-col justify-center items-center w-full h-auto lg:h-47.5 mt-4 lg:mt-64 gap-4'>
                     <div className='h-19 w-277.5 flex justify-center items-center flex-col gap-2'>
-                        <motion.h1 initial={{opacity:0 , y:-100}} whileInView={{ opacity: 1, y: 0 }}
-                        viewport={{ once: true, amount: 0.3 }} transition={{duration:1 , ease:'easeOut' ,delay:0.3}} className='text-text-color text-[36px] font-semibold leading-11'>Our Clients</motion.h1>
-                        <motion.p initial={{opacity:0 , y:100}} whileInView={{ opacity: 1, y: 0 }}
-                        viewport={{ once: true, amount: 0.3 }} transition={{duration:1 , ease:'easeOut' ,delay:0.5}} className='text-p-color text-[16px] leading-6 font-normal'>We have been working with some Fortune 500+ clients</motion.p>
+                      <motion.h1 initial={{opacity:0 , y:-100}} whileInView={{ opacity: 1, y: 0 }}
+                      viewport={{ once: true, amount: 0.3 }} transition={{duration:1 , ease:'easeOut' ,delay:0.3}} className='text-text-color text-2xl md:text-4xl lg:text-[36px] font-semibold leading-11'>Our Clients</motion.h1>
+                      <motion.p initial={{opacity:0 , y:100}} whileInView={{ opacity: 1, y: 0 }}
+                      viewport={{ once: true, amount: 0.3 }} transition={{duration:1 , ease:'easeOut' ,delay:0.5}} className='text-p-color text-[11px] md:text-[16px] leading-6 font-normal'>We have been working with some Fortune 500+ clients</motion.p>
                     </div>
-                    <motion.div  initial={{opacity:0 , x:-100}} whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true, amount: 0.3 }} transition={{duration:1 , ease:'easeOut' ,delay:1.2}} className='w-6xl h-24.5 flex flex-row justify-between items-center'>
+                    <motion.div  initial={{opacity:0 , x:-100}} whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true, amount: 0.3 }} transition={{duration:1 , ease:'easeOut' ,delay:1.2}} className='md:w-6xl h-24.5 flex flex-row lg:justify-evenly  md:justify-center xl:w-6xl xl:gap-34 md:gap-12 lg:gap-0 items-center justify-center w-78 [@media(min-width:375px)_and_(max-width:768px)]:w-full [@media(min-width:375px)_and_(max-width:768px)]:gap-3 [@media(min-width:500px)_and_(max-width:768px)]:justify-evenly lg:w-full  '>
                         {image.map((m)=>{
                         return(
                             <div key={m.id}>
@@ -93,25 +93,25 @@ export default function SectionOne() {
                         })}
                     </motion.div>
                     <Reveal animation='left'>
-                        <motion.div initial={{ opacity: 0, y: 100 }} whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true, amount: 0.3 }} transition={{ duration: 1, ease: "easeOut", delay: 0.3 }} className='h-104 w-360 gap-4 flex flex-col justify-center items-center mt-11'>
+                        <motion.div initial={{ opacity: 0, y: 100 }} whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true, amount: 0.3 }} transition={{ duration: 1, ease: "easeOut", delay: 0.3 }} className='lg:h-104 h-auto w-full lg:w-360 gap-24 md:gap-4 flex flex-col  justify-center items-center mt-11  '>
                             <motion.div initial={{ opacity: 0, y: 100 }} whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true, amount: 0.3 }} transition={{ duration: 1, ease: "easeOut", delay: 0.4 }} className='h-30 w-360 justify-center flex flex-col gap-2 items-center text-center'>
-                                <h1 className='text-text-color w-135.5 h-22 text-4xl leading-11 font-semibold'>Manage your entire community in a single system</h1>
+                                <h1 className='text-text-color md:w-135.5 md:h-22 text-2xl w-78 h-auto md:text-4xl leading-11 font-semibold'>Manage your entire community in a single system</h1>
                                 <p className='text-p-color h-6 w-360 text-[16px] leading-6'>Who is Nextcent suitable for?</p>
                             </motion.div>
-                            <motion.div initial={{ opacity: 0, x: -100 }} whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true, amount: 0.3 }} transition={{ duration: 1, ease: "easeOut", delay: 0.8 }} className='flex flex-col justify-center items-center md:flex-row md:w-360 md:h-70 md:justify-evenly'>
-                                <motion.div initial={{opacity:0 , x:-100}}    transition={{ duration: 1, ease: "easeOut", delay: 0.5 }} whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true, amount: 1 }} className='w-74.75 h-65 rounded-lg gap-2 flex flex-col justify-center items-center text-center  shadow-[0_2px_4px_0_#ABBED133]'>
-                                    <img src={img8} alt="" className='w-[65] h-14' />
+                            <motion.div initial={{ opacity: 0, x: -100 }} whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true, amount: 0.3 }} transition={{ duration: 1, ease: "easeOut", delay: 0.8 }} className='flex flex-col justify-center items-center md:flex-col gap-4 md:gap-8  lg:w-[75%] xl:w-full  lg:flex-row lg:gap-1   lg:h-70  lg:justify-evenly'>
+                                <motion.div initial={{opacity:0 , x:-100}}    transition={{ duration: 1, ease: "easeOut", delay: 0.5 }} whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true, amount: 1 }} className='w-74.75 h-65 rounded-lg gap-2 flex flex-col md:w-112.5 md:h-87.5 lg:w-74.75 lg:h-65 justify-center items-center text-center  shadow-[0_2px_4px_0_#ABBED133]'>
+                                    <img src={img8} alt="" className='w-16.25 md:w-[40%] lg:w-16.25 md:h-32 lg:h-14 h-14' />
                                     <h1 className='text-text-color h-18 w-66.75 font-bold text-[28px] leading-9.5'>Membership Organisations</h1>
                                     <p className='text-p-color w-62.75 h-15 text-sm leading-5 font-normal '>Our membership management software provides full automation of membership renewals and payments</p>
                                 </motion.div>
-                                <motion.div initial={{ opacity: 0, x: -100 }} whileInView={{ opacity: 1, x: 0 }}  viewport={{ once: true, amount: 0.3 }} transition={{ duration: 1, ease: "easeOut", delay: 1.5 }} className='w-74.75 h-65 rounded-lg gap-2 flex flex-col justify-center items-center text-center shadow-[0_2px_4px_0_#ABBED133]'>
-                                    <img src={img9} alt="" className='w-[65] h-14' />
+                                <motion.div initial={{ opacity: 0, x: -100 }} whileInView={{ opacity: 1, x: 0 }}  viewport={{ once: true, amount: 0.3 }} transition={{ duration: 1, ease: "easeOut", delay: 1.5 }} className='w-74.75 h-65 rounded-lg gap-2 md:w-112.5 md:h-87.5 lg:w-74.75 lg:h-65 flex flex-col justify-center items-center text-center shadow-[0_2px_4px_0_#ABBED133]'>
+                                    <img src={img9} alt="" className='w-16.25 md:w-[40%] lg:w-16.25 md:h-32 lg:h-14 h-14' />
                                     <h1 className='text-text-color h-18 w-62.5 text-center font-bold text-[28px] leading-9.5'>National Associations</h1>
                                     <p className='text-p-color w-60 h-20 text-sm leading-5 font-normal '>Our membership management software provides full automation of membership renewals and payments</p>
                                 </motion.div>
-                                <motion.div initial={{ opacity: 0, x: -100 }} whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true, amount: 0.3 }} transition={{ duration: 1, ease: "easeOut", delay: 2 }} className='w-74.75 h-65 rounded-lg gap-2 flex flex-col justify-center items-center text-center shadow-[0_2px_4px_0_#ABBED133]'>
-                                    <img src={img10} alt="" className='w-[65] h-14' />
-                                    <h1 className='text-text-color h-18 w-55  text-[28px] leading-9.5 font-bold'>Clubs And Groups</h1>
+                                <motion.div initial={{ opacity: 0, x: -100 }} whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true, amount: 0.3 }} transition={{ duration: 1, ease: "easeOut", delay: 2 }} className='w-74.75 h-65 rounded-lg gap-2 md:w-112.5 md:h-87.5 lg:w-74.75 lg:h-65 flex flex-col justify-center items-center text-center shadow-[0_2px_4px_0_#ABBED133]'>
+                                    <img src={img10} alt="" className='w-16.25 md:w-[40%] lg:w-16.25 md:h-32 lg:h-14 h-14' />
+                                    <h1 className='text-text-color h-18 w-55 md:  text-[28px] leading-9.5 font-bold'>Clubs And Groups</h1>
                                     <p className='text-p-color w-62.75 h-15 text-sm leading-5 font-normal '>Our membership management software provides full automation of membership renewals and payments</p>
                                 </motion.div>
                             </motion.div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,16 +14,16 @@ export default function Footer() {
         <Reveal>
             <footer className="w-full h-157 flex flex-col justify-center items-center  mt-20 ">
                 <div className="w-full h-75  gap-8 bg-bg-color flex flex-col justify-center items-center pb-3.5">
-                    <motion.h1 whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true, amount: 0.3 }} initial={{opacity:0 , x:-100}}   transition={{duration:1 , ease:'easeOut' ,delay:0.5}}  className="text-[64px] w-221.75 h-38 leading-19 font-semibold text-center text-[#263238]">Pellentesque suscipit fringilla libero eu.</motion.h1>
+                    <motion.h1 whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true, amount: 0.3 }} initial={{opacity:0 , x:-100}}   transition={{duration:1 , ease:'easeOut' ,delay:0.5}}  className="text-2xl lg:text-[64px] md:text-5xl w-full h-auto lg:w-221.75 lg:h-38 leading-19 font-semibold text-center text-[#263238]">Pellentesque suscipit fringilla libero eu.</motion.h1>
                     <span className="w-44.5 h-13 rounded-sm gap-2 bg-main-green text-white justify-center items-center flex flex-row cursor-pointer hover:bg-green-800 active:bg-green-800 hover:duration-500 duration-500 transition-all">Get a Demo<img src={share} alt="" srcset="" className="w-4 h-4 " /></span>
                 </div>
-                <div className="bg-footer-color w-full h-82 gap-31.25 flex flex-col md:flex-row md:justify-center md:items-center ">
-                    <div className="w-87.5 h-47.5 gap-10 flex flex-col items-start">
-                        <motion.img src={logo} whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true, amount: 0.3 }} initial={{opacity:0 , x:-100}}   transition={{duration:1 , ease:'easeOut' ,delay:0.5}} className="w-47.75 h-[29.67px] gap-2.5" alt="" srcset="" />
-                        <div className="w-87.5 h-12 gap-2 flex flex-col text-start">
+                <div className="bg-footer-color w-full h-1000 md:h-82 md:gap-31.25  lg:gap-2 xl:gap.31.25 flex flex-col md:flex-row md:justify-center md:items-center ">
+                    <div className="w-87.5 h-47.5 gap-10 flex flex-col items-start ml-3 md:pl-48  lg:pl-10  md:ml-0">
+                        <motion.img src={logo} whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true, amount: 0.3 }} initial={{opacity:0 , x:-100}}   transition={{duration:1 , ease:'easeOut' ,delay:0.5}} className="md:w-47.75 mt-5 md:mt-0 h-6.5 w-35 md:h-[29.67px] gap-2.5" alt="" srcset="" />
+                        <div className="w-87.5 h-12 gap-2 flex flex-col text-start  ">
                             <motion.p className="text-bg-color font-normal text-sm leading-5 h-5 w-87.5" whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true, amount: 0.3 }} initial={{opacity:0 , x:-100}}   transition={{duration:1 , ease:'easeOut' ,delay:0.5}} >Copyright © 2020 Nexcent ltd.</motion.p>
                             <motion.p className="text-bg-color font-normal text-sm leading-5 h-5 w-87.5" whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true, amount: 0.3 }} initial={{opacity:0 , x:-100}}   transition={{duration:1 , ease:'easeOut' ,delay:0.8}} >All rights reserved</motion.p>
-                            <div className="w-44 h-8 gap-4 flex flex-row">
+                            <div className="md:w-44 md:h-8 w-full h-auto gap-4  flex flex-row">
                                 <motion.div initial={{opacity:0 , x:-100}} whileInView={{ opacity: 1, x: 0 }}
                                     viewport={{ once: true, amount: 0.3 }}  transition={{duration:1 , ease:'easeOut' , delay:'0.3'}} className="w-8 h-8 rounded-full flex items-center justify-center bg-white/10 cursor-pointer">
                                     <img  src={instgram} alt="" className="w-4 h-4" />
@@ -46,7 +46,7 @@ export default function Footer() {
                             </div>
                         </div>
                     </div>
-                    <motion.div whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true, amount: 0.3 }} initial={{opacity:0 , x:-100}}   transition={{duration:1 , ease:'easeOut' ,delay:0.5}} className="w-158.75 h-50 gap-7.5 flex flex-row">
+                    <motion.div whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true, amount: 0.3 }} initial={{opacity:0 , x:-100}}   transition={{duration:1 , ease:'easeOut' ,delay:0.5}} className="w-158.75 md:h-50 h-60 gap-7.5 flex flex-row ml-3 md:ml-0  ">
                         <motion.div whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true, amount: 0.3 }} initial={{opacity:0 , y:-100}}   transition={{duration:1 , ease:'easeOut' ,delay:0.5}} className="w-40 h-50 gap-6 flex flex-col">
                             <h1 className="text-white font-semibold text-[20px] leading-7 w-40 h-7">Company</h1>
                             <div className="w-40 h-37 gap-3 flex flex-col">
@@ -67,9 +67,10 @@ export default function Footer() {
                                 <motion.p whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true, amount: 0.3 }} initial={{opacity:0 , y:-100}}   transition={{duration:1 , ease:'easeOut' ,delay:2}}  className="text-bg-color w-40 h-5 font-normal text-sm leading-5 cursor-pointer hover:underline">Status</motion.p>
                             </div>
                         </motion.div>
-                        <motion.div whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true, amount: 0.3 }} initial={{opacity:0 , y:-100}}   transition={{duration:1 , ease:'easeOut' ,delay:1.2}} className="w-62.5 h-23 flex flex-col gap-6">
-                            <motion.h1 whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true, amount: 0.3 }} initial={{opacity:0 , y:-100}}   transition={{duration:1 , ease:'easeOut' ,delay:1.5}} className="text-white font-semibold text-[20px] leading-7 w-40 h-7">Stay up to date</motion.h1>
-                            <motion.label whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true, amount: 0.3 }} initial={{opacity:0 , y:-100}}   transition={{duration:1 , ease:'easeOut' ,delay:1.9}} className="relative w-64.75 h-10 block">
+                        <motion.div whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true, amount: 0.3 }} initial={{opacity:0 , y:-100}}   transition={{duration:1 , ease:'easeOut' ,delay:1.2}} 
+                        className="md:w-62.5 w-120 h-33  flex flex-col gap-6 mt-56 lg:mt-0  lg:right-6 xl:right-0 lg:pr-0 [@media(min-width:375px)_and_(max-width:1024px)]:pr-260 [@media(min-width:375px)_and_(max-width:1024px)]:pl-5 relative right-100 md:right-110 pr-15  md:-ml-64 lg:ml-0  pl-3 bg-footer-color">
+                            <motion.h1 whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true, amount: 0.3 }} initial={{opacity:0 , y:-100}}   transition={{duration:1 , ease:'easeOut' ,delay:1.5}} className="text-white font-semibold text-[20px] md:ml-7 lg:ml-0 leading-7 w-40 h-7">Stay up to date</motion.h1>
+                            <motion.label whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true, amount: 0.3 }} initial={{opacity:0 , y:-100}}   transition={{duration:1 , ease:'easeOut' ,delay:1.9}} className="relative w-64.75 h-10 md:ml-7 lg:ml-0 flex">
                                 <img src={wing} alt="" className="w-4 h-4 absolute top-2.5 left-56.25"/>
                                 <input type="text" className="w-full h-full rounded-lg bg-white/10 text-[#D9DBE1] placeholder:text-[#D9DBE1] pl-4 outline-none" placeholder="Your email address"/>
                             </motion.label>
